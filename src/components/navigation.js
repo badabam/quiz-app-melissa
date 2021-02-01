db643666d836988b1f9ce202a380fe84b57e1bb9
Jeremias Erbs <jerry@badabam.de>
2021-02-01 14:51:59 +0100

    Use forEach to simplify navgiation code

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,61 +1,22 @@
-import getByDataJs from '../js/getByDataJs'
+import getAllByDataJs from '../js/getAllByDataJs'
 
 export default function navigation() {
-  const pagehome = getByDataJs('pagehome')
-  const pagebookmark = getByDataJs('pagebookmark')
-  const pagecreate = getByDataJs('pagecreate')
-  const pageprofile = getByDataJs('pageprofile')
+  const pages = getAllByDataJs('page')
+  const navButtons = getAllByDataJs('nav')
 
-  const iconhome = getByDataJs('iconhome')
-  const iconbookmark = getByDataJs('iconbookmark')
-  const iconcreate = getByDataJs('iconreate')
-  const iconprofile = getByDataJs('iconprofile')
-
-  iconhome.addEventListener('click', () => {
-    pagehome.hidden = false
-    pagebookmark.hidden = true
-    pagecreate.hidden = true
-    pageprofile.hidden = true
-
-    iconhome.classList.add('active-icon')
-    iconbookmark.classList.remove('active-icon')
-    iconcreate.classList.remove('active-icon')
-    iconprofile.classList.remove('active-icon')
-  })
-
-  iconbookmark.addEventListener('click', () => {
-    pagehome.hidden = true
-    pagebookmark.hidden = false
-    pagecreate.hidden = true
-    pageprofile.hidden = true
-
-    iconhome.classList.remove('active-icon')
-    iconbookmark.classList.add('active-icon')
-    iconcreate.classList.remove('active-icon')
-    iconprofile.classList.remove('active-icon')
-  })
-
-  iconcreate.addEventListener('click', () => {
-    pagehome.hidden = true
-    pagebookmark.hidden = true
-    pagecreate.hidden = false
-    pageprofile.hidden = true
-
-    iconhome.classList.remove('active-icon')
-    iconbookmark.classList.remove('active-icon')
-    iconcreate.classList.add('active-icon')
-    iconprofile.classList.remove('active-icon')
-  })
-
-  iconprofile.addEventListener('click', () => {
-    pagehome.hidden = true
-    pagebookmark.hidden = true
-    pagecreate.hidden = true
-    pageprofile.hidden = false
-
-    iconhome.classList.remove('active-icon')
-    iconbookmark.classList.remove('active-icon')
-    iconcreate.classList.remove('active-icon')
-    iconprofile.classList.add('active-icon')
+  navButtons.forEach(button => {
+    const clickedButtonName = button.dataset.name // <button data-js="nav" data-name="home">Home</button>
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.name // <section data-js="page" data-name="home">...</section>
+        page.classList.toggle('hidden', clickedButtonName !== pageName)
+      })
+      navButtons.forEach(button => {
+        button.classList.toggle(
+          'active',
+          clickedButtonName === button.dataset.name
+        )
+      })
+    })
   })
 }
